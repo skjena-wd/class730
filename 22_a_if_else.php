58c9a511e3a14227ba745a9a183ba5136aae0d8a
sushanta kumar jena <sushantakumarjena@sushantas-MacBook-Air.local>
2023-05-14 08:10:38 +0530

    if else elseif control structure explained

---
+++ 22_a_if_else.php
@@ -0,0 +1,17 @@
+<?php
+
+   $a = 50; 
+   $b = 50;
+
+   if($a>$b){
+      echo $a . " is greater than " . $b;
+   }
+    else if($a == $b) 
+   {
+      echo $a . " is equal to " . $b;
+   }
+   else {
+      echo $a . " is smaller than " . $b;
+   }
+
+?>
---
+++ 22_control_structure.php
@@ -0,0 +1,25 @@
+<?php
+// if ... 
+// else
+// for
+// foreach
+// while ... do while
+
+// include
+// include_once
+// require
+// require_once
+// break
+
+// switch ... case
+// continue
+// goto
+// define
+// declare
+
+
+
+
+
+
+?>
