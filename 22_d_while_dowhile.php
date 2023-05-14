b3a1304fb0faf3f0d5c05f7f07564139aad9626a
sushanta kumar jena <sushantakumarjena@sushantas-MacBook-Air.local>
2023-05-14 08:31:48 +0530

    Control Structure while and do-while explained

---
+++ 22_d_while_dowhile.php
@@ -0,0 +1,17 @@
+<?php
+$a = 5;
+
+while($a <10){
+   echo "<h3>" . $a ."</h3>";
+   $a++;
+}
+
+// do while loop
+$b = 5;
+do{
+   echo "<h3>The number is : " . $b ."</h3>";
+   $b++;
+}while($b <= 10)
+
+
+?>
